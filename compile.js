70c23577a587d6cc89b33e586a99358ae6ac496c
Jr <jr@ailleurs.me>
2012-10-20 15:28:17 -0700

    improved the compile workflow

--- compile.js
+++ compile.js
@@ -1,8 +1,15 @@
+#!/usr/bin/env node
+
 var fs = require("fs");
+// https://github.com/srod/node-minify
 
 var tmpl = fs.readFileSync('template.html');
 tmpl = tmpl.toString();
 tmpl = tmpl.replace(/\n/g, '\\\n');
 tmpl = tmpl.replace(/"/g, '\\"');
 
-fs.appendFileSync('inject.js', '\n// template\ntmpl = \"' + tmpl + '\";\n');
+var injectFile = fs.createReadStream('inject.js');
+injectFile.pipe(fs.createWriteStream('inject.compiled.js'));
+injectFile.on("end", function() {
+    fs.appendFileSync('inject.compiled.js', '\n// template\ntmpl = \"' + tmpl + '\";\n');
+});
--- inject.compiled.js
+++ inject.compiled.js
@@ -9,3 +9,18 @@ scriptTag.onload = function(){
     $('body').append(tmpl);
     $('h1').text(old);
 };
+
+// template
+tmpl = "<!DOCTYPE html>\
+<html>\
+<head>\
+    <title>dotCloud Node.js &amp; MongoDB Hello Worlds stack</title>\
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"https://dashboard.dotcloud.com/css/index.css\">\
+    <style type=\"text/css\">\
+    </style>\
+</head>\
+<body>\
+    <h1></h1>\
+    <h3>Push Again</h3>\
+</body>\
+</html>";
