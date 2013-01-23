0ed55245ec2173e85bb6024aecad8b7e76180bf9
Jr <jr@ailleurs.me>
2013-01-22 16:35:51 -0800

    cleaning

--- compile.js
+++ compile.js
@@ -8,21 +8,21 @@ var tmpl = fs.readFileSync('template.html');
 tmpl = tmpl.toString();
 
 var injectStream = fs.createReadStream('inject.js');
-injectStream.pipe(fs.createWriteStream('inject.compiled.js'));
+injectStream.pipe(fs.createWriteStream('compiled/inject.compiled.js'));
 injectStream.on("end", function() {
     // Read and grab the templates parts
     $ = cheerio.load(tmpl);
     var head = $('head').html().trim().replace(/\n/g, '\\\n').replace(/"/g, '\\"');
     var body = $('body').html().trim().replace(/\n/g, '\\\n').replace(/"/g, '\\"');
     // Inlines the template
-    fs.appendFileSync('inject.compiled.js', '\n// template\n');
-    fs.appendFileSync('inject.compiled.js', 'tmpl.head = \"' + head + '\";\n');
-    fs.appendFileSync('inject.compiled.js', 'tmpl.body = \"' + body + '\";\n');
+    fs.appendFileSync('compiled/inject.compiled.js', '\n// template\n');
+    fs.appendFileSync('compiled/inject.compiled.js', 'tmpl.head = \"' + head + '\";\n');
+    fs.appendFileSync('compiled/inject.compiled.js', 'tmpl.body = \"' + body + '\";\n');
     // Minify
     new compressor.minify({
         type: 'uglifyjs',
-        fileIn: 'inject.compiled.js',
-        fileOut: 'inject.min.js',
+        fileIn: 'compiled/inject.compiled.js',
+        fileOut: 'compiled/inject.min.js',
         callback: function(err){
             if(err) console.log(err);
         }
